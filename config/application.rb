dcc637802e3d06e4a2e97300c1b824ee91bda3e3
Safiya Aideed <gcab022021@coderacademy.edu.au>
2021-05-27 17:45:09 +1000

    config

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,9 @@ module SoleMate
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    
+    config.assets.initialize_on_precompile = false 
+    
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
